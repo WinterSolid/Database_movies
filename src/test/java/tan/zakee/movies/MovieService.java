a16c12e22dd5745e1055fb006a485f7373d75780
wintersolid <ztanksley@luc.edu>
2024-01-06 09:28:31 -0800

    anotate Autowired to instantiate constructors for MovieRepository.

--- src/test/java/tan/zakee/movies/MovieService.java
+++ src/test/java/tan/zakee/movies/MovieService.java
@@ -1,14 +1,18 @@
 package tan.zakee.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class MovieService {
 
+    @Autowired  // instantiate constructors for class MovieRepo...
     private MovieRepository movieRepository;
 
     public List<Movie> allMovies() {
+        return movieRepository.findAll();
+
 
     }
 }
